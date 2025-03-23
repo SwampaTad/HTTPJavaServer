054f69658868b6c1123de2e75c0563bb66ef953f
SwampaTad <tadeu.gollnick@gmail.com>
2025-03-23 15:46:53 +0000

    Finished creating an HTTP Server that accepts a connection on port 8080 and displays current date.

--- SimpleHTTPServer.java
+++ SimpleHTTPServer.java
@@ -1,23 +1,20 @@
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.Date;
+
 public class SimpleHTTPServer {
 
-    public static void main(String[] args) throws Exception {
-        final ServerSocket server = new ServerSocket(8080);
+    public static void main(String args[]) throws IOException{
+
+        ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080");
-        while(true){
-            Socket clientSocket = server.accept();
-            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader reader = new BufferedReader(isr);
-            String line = reader.readLine();
-            while(!line.isEmpty()){
-                System.out.println(line);
-                line = reader.readLine();
+        while (true){
+            try(Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
             }
-            
         }
     }
 }
