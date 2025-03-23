939156eaa34a2aa8a4bd9c983fadfcdc6504fbbc
SwampaTad <tadeu.gollnick@gmail.com>
2025-03-23 15:40:35 +0000

    Improved HTTP Server so that it accepts clients requests and displays data about browser in command line.

--- SimpleHTTPServer.java
+++ SimpleHTTPServer.java
@@ -1,11 +1,23 @@
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 public class SimpleHTTPServer {
 
     public static void main(String[] args) throws Exception {
         final ServerSocket server = new ServerSocket(8080);
-        System.out.println("Listening for connection on port 8080")
+        System.out.println("Listening for connection on port 8080");
         while(true){
-            // spin forever
+            Socket clientSocket = server.accept();
+            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            while(!line.isEmpty()){
+                System.out.println(line);
+                line = reader.readLine();
+            }
+            
         }
     }
 }
